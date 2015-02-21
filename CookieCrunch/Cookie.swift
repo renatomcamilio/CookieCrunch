2a85c99c30bcf28329041ac53ed0465931c9ce64
Renato Camilio <renatomcamilio@gmail.com>
2015-02-20 18:20:51 -0800

    Cookie Model, CookieType enum representation of the cookies types, as well as convenience methods for accessing them in a more human readable format

---
+++ CookieCrunch/Cookie.swift
@@ -0,0 +1,50 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Renato Camilio on 2/20/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"]
+        
+        return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    
+    static func random() -> CookieType {
+        return CookieType(rawValue: Int(arc4random_uniform(UInt32(CookieType.SugarCookie.rawValue))))!
+    }
+}
+
+class Cookie: Printable {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+    
+    var description: String {
+        return "type: \(cookieType) square:[\(column),\(row)]"
+    }
+}
