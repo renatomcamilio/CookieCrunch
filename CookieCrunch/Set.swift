0b56a48935869286b36874e5cdf418218dffb15a
Renato Camilio <renatomcamilio@gmail.com>
2015-02-22 00:23:42 -0800

    Adding Set Type

---
+++ CookieCrunch/Set.swift
@@ -0,0 +1,54 @@
+//
+//  Set.swift
+//  CookieCrunch
+//
+//  Created by Renato Camilio on 2/21/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+struct Set<T: Hashable>: SequenceType, Printable {
+    
+    private var dictionary = [T:Bool]()
+    
+    mutating func addElement(newElement: T) {
+        dictionary[newElement] = true
+    }
+    
+    mutating func removeElement(element: T) {
+        dictionary[element] = nil
+    }
+    
+    func containsElement(element: T) -> Bool {
+        return dictionary[element] != nil
+    }
+    
+    func allElements() -> [T] {
+        return Array(dictionary.keys)
+    }
+    
+    var count: Int {
+        return dictionary.count
+    }
+    
+    func unionSet(otherSet: Set<T>) -> Set<T> {
+        var combined = Set<T>()
+        
+        for obj in dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+        
+        for obj in otherSet.dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+        
+        return combined
+    }
+    
+    func generate() -> IndexingGenerator<Array<T>> {
+        return allElements().generate()
+    }
+    
+    var description: String {
+        return dictionary.description
+    }
+}
